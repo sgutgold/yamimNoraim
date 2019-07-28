79b97c0a5a44e1247c33f707b1d9575d64f5bd21
sgutgold <sgutgold@users.noreply.github.com>
2019-07-28 11:46:18 +0300

    --dbg

--- server.js
+++ server.js
@@ -817,7 +817,7 @@ initialize();
 
          /*     init new files in debug  */
 
-	localFileDir='data/';
+	localFileDir='';    //'data/';
 	//localFileDir='D_';
 		 
 XLSXfilename=localFileDir	+'membersRequests.xlsx';  
